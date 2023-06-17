814c815062aeaa6a76fb56ade0ec6b6875b40a96
Brent Buchholz <brentbuchholz@gmail.com>
2023-06-17 09:19:17 -0500

    Copy AWLWA main list

---
+++ src/pages/hi/AWLWA.vue
@@ -0,0 +1,120 @@
+<template>
+  <h1> AWLWA CarteoGraphy </h1>
+
+  <table class="tg">
+    <thead>
+      <tr>
+        <th class="tg-baqh" colspan="2">
+          <span style="font-weight:400;font-style:normal">🜄</span>
+        </th>
+        <th class="tg-baqh" />
+        <th class="tg-baqh" colspan="2">
+          🜂
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="tg-baqh">
+          <router-link to="4">
+            4
+          </router-link>
+        </td>
+        <td class="tg-baqh">
+          <router-link to="5">
+            5
+          </router-link>
+        </td>
+        <td class="tg-baqh" />
+        <td class="tg-baqh">
+          <router-link to="8">
+            8
+          </router-link>
+        </td>
+        <td class="tg-baqh">
+          <router-link to="1">
+            1
+          </router-link>
+        </td>
+      </tr>
+      <tr>
+        <td class="tg-baqh">
+          <router-link to="6">
+            6
+          </router-link>
+        </td>
+        <td class="tg-baqh">
+          <router-link to="3">
+            3
+          </router-link>
+        </td>
+        <td class="tg-baqh" />
+        <td class="tg-baqh">
+          <router-link to="2">
+            2
+          </router-link>
+        </td>
+        <td class="tg-baqh">
+          <router-link to="7">
+            7
+          </router-link>
+        </td>
+      </tr>
+      <tr>
+        <td class="tg-baqh">
+          <router-link to="2">
+            2
+          </router-link>
+        </td>
+        <td class="tg-baqh">
+          <router-link to="7">
+            7
+          </router-link>
+        </td>
+        <td class="tg-baqh" />
+        <td class="tg-baqh">
+          <router-link to="6">
+            6
+          </router-link>
+        </td>
+        <td class="tg-baqh">
+          <router-link to="3">
+            3
+          </router-link>
+        </td>
+      </tr>
+      <tr>
+        <td class="tg-baqh">
+          <router-link to="8">
+            8
+          </router-link>
+        </td>
+        <td class="tg-baqh">
+          <router-link to="1">
+            1
+          </router-link>
+        </td>
+        <td class="tg-baqh" />
+        <td class="tg-baqh">
+          <router-link to="4">
+            4
+          </router-link>
+        </td>
+        <td class="tg-baqh">
+          <router-link to="5">
+            5
+          </router-link>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+  <style type="text/css">
+  .tg  {border-collapse:collapse;border-spacing:0;}
+  .tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+    overflow:hidden;padding:10px 5px;word-break:normal;}
+  .tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+    font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+  .tg .tg-baqh{text-align:center;vertical-align:top}
+  </style>
